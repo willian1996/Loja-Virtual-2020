8429eca2c5342d5892911701ef6fc39d90bea0f4
Willian Sales Gabriel <williansalesgabriel@hotmail.com>
2020-12-29 00:14:38 -0300

    Atualização 29/12

--- sistema/painel-admin/index.php
+++ sistema/painel-admin/index.php
@@ -113,7 +113,7 @@ $pdo->query("UPDATE produtos SET promocao = 'Sim' where id = $id_pro");
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
